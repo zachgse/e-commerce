bf70d6160ce82c8ee3db3545755dc87e50c45325
Zach Estrella <zachgabriel.estrella@ziaplex.biz>
2025-12-21 23:58:56 +0800

    Add color for modal loading

--- frontend/src/components/reusable/ModalLoading.tsx
+++ frontend/src/components/reusable/ModalLoading.tsx
@@ -12,7 +12,7 @@ const ModalLoading = (props:ModalLoadingProps) => {
         <>
             <div className="fixed inset-0 bg-black opacity-50 z-30"></div>
             <div className="fixed inset-0 flex justify-center items-center z-40">
-                <Loading/>
+                <Loading color='aqua'/>
             </div>
         </>
     )
